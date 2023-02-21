e34290de29f08e70e25231a07a3fd470ac19070b
gilgim <gilgim21@gmail.com>
2023-02-21 16:54:04 +0900

    Initial Commit

---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by Gaea on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
